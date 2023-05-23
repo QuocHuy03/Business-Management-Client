073db7b5e3c9f3e5a5fd2c1530718da0b8471755
quốc huy lê <huylquoc19102003@gmail.com>
2023-05-23 14:01:46 +0700

    23/5

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  
+  constructor(private authService: AuthService, private router: Router) {}
+  logout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('level');
+    this.authService.setLevel(null);
+    this.router.navigate(['/login']);
+  }
 }
