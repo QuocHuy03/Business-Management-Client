1bf69ab1e6f1aed1f878fb892296f80959831142
quốc huy lê <huylquoc19102003@gmail.com>
2023-05-23 12:03:13 +0700

    23/5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ProjectService } from '../services/project.service';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-home',
@@ -12,21 +13,35 @@ export class HomeComponent implements OnInit {
   listAllProjects: any;
   listIDProject: any;
   lengthProjectDeployment: any;
+  lengthTaskProcessing: any;
+  totalReducerProject: any;
+
+  filterMembersEast: any;
+  filterMembersWest: any;
+  filterMembersSouth: any;
+  filterMembersNorth: any;
 
   constructor(
     private authService: AuthService,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private taskService: TaskService
   ) {}
 
   ngOnInit(): void {
     this.getAllUsers();
     this.getAllProjects();
+    this.getAllTasks();
   }
 
   getAllUsers() {
     this.authService.getUsers().subscribe(
       (response) => {
-        this.totalAllUsers = response;
+        // console.log(response)
+        this.totalAllUsers = response.length;
+        this.filterMembersEast = response.filter((huyne: any) => huyne.area.nameArea === "East");
+        this.filterMembersWest = response.filter((huyne: any) => huyne.area.nameArea === "West");
+        this.filterMembersSouth = response.filter((huyne: any) => huyne.area.nameArea === "South");
+        this.filterMembersNorth = response.filter((huyne: any) => huyne.area.nameArea === "North");
       },
       (error) => {
         console.log(error);
@@ -34,13 +49,28 @@ export class HomeComponent implements OnInit {
     );
   }
 
-
+  getAllTasks() {
+    this.taskService.get().subscribe(
+      (response) => {
+        // console.log(response)
+        this.lengthTaskProcessing = response.filter((huyne: any) => huyne.status === "processing").length;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  
 
   getAllProjects() {
     this.projectService.get().subscribe(
       (response) => {
-        console.log(response);
-        this.lengthProjectDeployment =response.filter((huyne: any) => huyne.status === "deployment").length;
+        // console.log(response);
+        this.lengthProjectDeployment = response.filter((huyne: any) => huyne.status === "deployment").length;
+        this.totalReducerProject = response.reduce((total: any, item: any) => 
+          total + parseInt(item.budget), 0
+        );
+
         this.listAllProjects = response;
         const selectedValue = this.listAllProjects[0]._id;
         this.getSelectedProjects(selectedValue);
@@ -64,7 +94,7 @@ export class HomeComponent implements OnInit {
 
   onProjectChange(event: any) {
     const selectedValue = event.target.value;
-    console.log(selectedValue);
+    // console.log(selectedValue);
     this.getSelectedProjects(selectedValue);
   }
 }
